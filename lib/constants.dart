7a0abcbe0cbde734365d2050a700ccdd595b27d9
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-17 18:04:37 +0200

    Create

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+  // Colors
+  // ignore: non_constant_identifier_names
+  Color PrimaryColor = Colors.white;
+  // ignore: non_constant_identifier_names
+  Color TextColor = Colors.black;
+  // ignore: non_constant_identifier_names
+  Color HoverColor = Colors.blue.shade700;
