382bdec0e8226cf162a32a5b58aa033bd3b7343e
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-17 18:05:02 +0200

    Update

--- lib/pages/home/homeDesktop.dart
+++ lib/pages/home/homeDesktop.dart
@@ -1,14 +1,22 @@
 import 'package:erb/widget/appBar/appBar.dart';
 import 'package:flutter/material.dart';
+import 'package:erb/constants.dart';
 
 class HomeDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: PrimaryColor,
       appBar: PreferredSize(
         preferredSize: Size(width, 70),
         child: CustomAppBar(),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+          ],
+        ),
       ),
     );
   }
