5147b01c7434efbfeb687f8dd6686445ecb03d6c
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-17 09:33:31 +0200

    Update

--- lib/widget/appBar/appBar.dart
+++ lib/widget/appBar/appBar.dart
@@ -4,15 +4,59 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: 50,
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 100,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'assets/logo.png',
+            ),
+            Spacer(
+              flex: 2,
+            ),
+            Text(
+              'Apps',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              'Pricing',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              'Support',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              'Sign In',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+            ),
+          ],
+        ),
       ),
-          child: Row(
-            children: [
-              Image.asset('assets/logo.png'),
-            ],
-          ),
-    ));
+    );
   }
 }
