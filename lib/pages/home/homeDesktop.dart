8f7dbb04a7f6605efa3bdcac0e1a1fec89e2d501
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-18 20:38:40 +0200

    Update

--- lib/pages/home/homeDesktop.dart
+++ lib/pages/home/homeDesktop.dart
@@ -14,8 +14,7 @@ class HomeDesktop extends StatelessWidget {
       ),
       body: Container(
         child: Column(
-          children: [
-          ],
+          children: [],
         ),
       ),
     );
