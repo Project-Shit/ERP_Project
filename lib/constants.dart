e3ccfd9401d8d090cda82c889546cfbeeea95445
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-18 20:38:36 +0200

    Update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
-  // Colors
-  // ignore: non_constant_identifier_names
-  Color PrimaryColor = Colors.white;
-  // ignore: non_constant_identifier_names
-  Color TextColor = Colors.black;
-  // ignore: non_constant_identifier_names
-  Color HoverColor = Colors.blue.shade700;
+// Colors
+// ignore: non_constant_identifier_names
+Color PrimaryColor = Colors.white;
+// ignore: non_constant_identifier_names
+Color TextColor = Colors.black;
+// ignore: non_constant_identifier_names
+Color HoverColor = Colors.blue.shade700;
+
+// Custom Button
+Widget appButton(String title, VoidCallback onTap) {
+  // ignore: deprecated_member_use
+  return FlatButton(
+    child: Text(
+      title,
+      style: TextStyle(
+        color: TextColor,
+        fontSize: 20,
+      ),
+    ),
+    hoverColor: HoverColor,
+    onPressed: onTap,
+    height: 70,
+  );
+}
