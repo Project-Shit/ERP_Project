def4f0f95949be4f421be552e852f344b7cce8bf
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-24 00:30:24 +0200

    Update

--- lib/pages/home/homeDesktop.dart
+++ lib/pages/home/homeDesktop.dart
@@ -1,6 +1,13 @@
 import 'package:erb/widget/appBar/appBar.dart';
+import 'package:erb/widget/footer/footer.dart';
 import 'package:flutter/material.dart';
 import 'package:erb/constants.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:footer/footer.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:footer/footer_view.dart';
 
 class HomeDesktop extends StatelessWidget {
   @override
@@ -13,55 +20,77 @@ class HomeDesktop extends StatelessWidget {
         preferredSize: Size(width, 70),
         child: CustomAppBar(),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: height * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/home.png',
-                  width: width * 0.3,
+      body: FooterView(
+        flex: 8,
+        children: [
+          SizedBox(
+            height: height * 0.05,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/home.png',
+                width: width * 0.3,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: height * 0.05,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Create your own System with Web Hosting',
+                style: TextStyle(
+                  color: TextColor,
+                  fontSize: width * 0.03,
                 ),
-              ],
-            ),
-            SizedBox(
-              height: height * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Create your own System with Web Hosting',
-                  style: TextStyle(
-                    color: TextColor,
-                    fontSize: width * 0.03,
-                  ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: height * 0.05,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Go Serverless and Pay only for what you use',
+                style: TextStyle(
+                  color: TextColor,
+                  fontSize: width * 0.02,
                 ),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: height * 0.06,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/down.png',
+                width: height * 0.1,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: height * 0.07,
+          ),
+          Container(
+            color: Colors.blue,
+            width: width,
+            height: 800,
+            child: Row(
+              children: [],
             ),
-            SizedBox(
-              height: height * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Go Serverless and Pay only for what you use',
-                  style: TextStyle(
-                    color: TextColor,
-                    fontSize: width * 0.02,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: height * 0.05,
-            ),
-          ],
+          ),
+        ],
+        footer: new Footer(
+          child: CustomFooter(),
         ),
       ),
     );
