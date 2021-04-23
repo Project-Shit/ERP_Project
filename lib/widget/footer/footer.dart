c85403eed74840fe37febf8fe170fc193eda3d49
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-24 00:30:08 +0200

    Update

--- lib/widget/footer/footer.dart
+++ lib/widget/footer/footer.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Footer extends StatelessWidget {
+class CustomFooter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      persistentFooterButtons: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/copyright.png',
-              width: 20,
-            ),
-            SizedBox(
-              width: 6,
-            ),
-            Text(
-              'All Right Reserved by Nerd\'s Team',
-            ),
-          ],
-        ),
-      ],
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      height: 30,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/copyright.png',
+            width: 20,
+          ),
+          SizedBox(
+            width: 6,
+          ),
+          Text(
+            'All Right Reserved by Nerd\'s Team',
+          ),
+        ],
+      ),
     );
   }
 }
