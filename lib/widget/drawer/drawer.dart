92d1dac96104d37832b1cc1f1637614e830140d9
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-30 00:03:20 +0200

    Update

---
+++ lib/widget/drawer/drawer.dart
@@ -0,0 +1,108 @@
+import 'package:erb/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: PrimaryColor,
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: Center(
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 230,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: WidgetColor,
+              ),
+            ),
+            ListTileTheme(
+              child: ListTile(
+                leading: Icon(
+                  Icons.home,
+                  color: TextColor,
+                ),
+                title: Text(
+                  'Home',
+                  style: TextStyle(
+                    color: TextColor,
+                    fontSize: 20,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            ListTileTheme(
+              child: ListTile(
+                leading: Icon(
+                  Icons.widgets_outlined,
+                  color: TextColor,
+                ),
+                title: Text(
+                  'Apps',
+                  style: TextStyle(
+                    color: TextColor,
+                    fontSize: 20,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            ListTileTheme(
+              child: ListTile(
+                leading: Icon(
+                  Icons.attach_money,
+                  color: TextColor,
+                ),
+                title: Text(
+                  'Pricing',
+                  style: TextStyle(
+                    color: TextColor,
+                    fontSize: 20,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            ListTileTheme(
+              child: ListTile(
+                leading: Icon(
+                  Icons.support_agent,
+                  color: TextColor,
+                ),
+                title: Text(
+                  'Support',
+                  style: TextStyle(
+                    color: TextColor,
+                    fontSize: 20,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            ListTileTheme(
+              child: ListTile(
+                leading: Icon(
+                  Icons.input_sharp,
+                  color: TextColor,
+                ),
+                title: Text(
+                  'Sign in',
+                  style: TextStyle(
+                    color: TextColor,
+                    fontSize: 20,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
