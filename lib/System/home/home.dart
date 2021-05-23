45a3b84b5c9364908c9c809d01104fe6c8361ba2
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-23 18:08:35 +0200

    Update

--- lib/System/home/home.dart
+++ lib/System/home/home.dart
@@ -1,5 +1,5 @@
-import 'package:erp/pages/home/homeDesktop.dart';
-import 'package:erp/pages/home/homeMobile.dart';
+import 'package:erp/System/home/homeDesktop.dart';
+import 'package:erp/System/home/homeMobile.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:responsive_builder/responsive_builder.dart';
