0010333907e96c76a361062067350b26e07345d6
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-23 18:08:45 +0200

    Update

--- lib/constants.dart
+++ lib/constants.dart
@@ -43,3 +43,43 @@ Widget labelButton(String title, VoidCallback onTap) {
     onPressed: onTap,
   );
 }
+
+Widget actionButtons(String title, VoidCallback onTap, Color color) {
+  return Container(
+    width: 300,
+    height: 70,
+    // ignore: deprecated_member_use
+    child: RaisedButton(
+      color: color,
+      padding: EdgeInsets.only(
+        top: 20,
+        bottom: 20,
+        left: 35,
+        right: 35,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+      ),
+      child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 30,
+              color: TextColor,
+            ),
+          ),
+      onPressed: onTap,
+    ),
+  );
+}
+
+Widget labelText(String title) {
+  return Text(
+    title,
+    style: TextStyle(
+      color: TextColor,
+      fontSize: 30,
+    ),
+  );
+}
