86a4c9284caaeaf38b84313803d7363667ff3ced
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-24 00:46:04 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // @dart=2.9
-import 'package:erp/Client/Accounting/Users/users.dart';
-import 'package:erp/System/home/home.dart';
+import 'package:erp/Client/Accounting/Tax/tax.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Users(),
+      home: Tax(),
     );
   }
 }
