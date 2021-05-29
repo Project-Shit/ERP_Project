97722decbebd4a388afc18b80e75db512606b8d4
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-29 11:46:23 +0200

    Update

---
+++ lib/Client/Accounting/Product/productDesktop.dart
@@ -0,0 +1,15 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+class ProductDesktop extends StatefulWidget {
+
+  @override
+  _ProductDesktopState createState() => _ProductDesktopState();
+}
+
+class _ProductDesktopState extends State<ProductDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
