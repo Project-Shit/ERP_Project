a106ba5ed596e5df6e1c636880e9558da626c625
EGYPT_LAPTOP <shadi.almancy.sa@gmail.com>
2021-05-30 22:30:31 +0200

    creating inventory

---
+++ lib/Inventory/inventory.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class inventory extends StatefulWidget {
+  @override
+  _inventoryState createState() => _inventoryState();
+}
+
+class _inventoryState extends State<inventory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hello"),
+    );
+  }
+}
