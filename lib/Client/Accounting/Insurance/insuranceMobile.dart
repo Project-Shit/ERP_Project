daa0a3738f1661a6af3d531b4dde561543456bf4
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 20:58:12 +0200

    Update

---
+++ lib/Client/Accounting/Insurance/insuranceMobile.dart
@@ -0,0 +1,130 @@
+// @dart=2.9
+import 'package:erp/constants.dart';
+import 'package:erp/widget/drawer/clientDrawer.dart';
+import 'package:flutter/material.dart';
+
+class InsuranceMobile extends StatefulWidget {
+
+  @override
+  _InsuranceMobileState createState() => _InsuranceMobileState();
+}
+
+class _InsuranceMobileState extends State<InsuranceMobile> {
+
+  List<String> _locations = [];
+  String _selectedLocation;
+  bool password = true;
+  final _textController = TextEditingController();
+
+  void setValue() {
+    String value = '';
+    setState(() {
+      _selectedLocation = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: PrimaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size(width, 60),
+        child: AppBar(
+          iconTheme: IconThemeData(
+            color: TextColor,
+          ),
+          backgroundColor: PrimaryColor,
+          title: Text(
+            'Company Name',
+            style: TextStyle(
+              color: TextColor,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+      drawer: ClientDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            width: width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                50.0,
+              ),
+              border: Border.all(
+                color: TextColor,
+                width: 2,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                bottom: 50,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  labelText('Employee ID'),
+                  dropList(_locations, _selectedLocation, width * 0.9, 45.0,
+                      setValue),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Employee Name'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('SSIN'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Social Number'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Balance'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Monthly payment'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Expenses'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Insurance Type'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Insurance Plan'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Hospital Name'),
+                  textField(_textController, width, 45.0, false),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
