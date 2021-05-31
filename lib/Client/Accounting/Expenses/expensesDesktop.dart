948621f51616e0dbe096919a6d34d737d82911a8
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 21:02:37 +0200

    Create

---
+++ lib/Client/Accounting/Expenses/expensesDesktop.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExpensesDesktop extends StatefulWidget {
+
+  @override
+  _ExpensesDesktopState createState() => _ExpensesDesktopState();
+}
+
+class _ExpensesDesktopState extends State<ExpensesDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
