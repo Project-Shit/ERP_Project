c94b4b26ef146b9ecaa2708009a3ae33098f3526
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 21:44:44 +0200

    Create

---
+++ lib/Client/Accounting/Company/companyDesktop.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CompanyDesktop extends StatefulWidget {
+
+  @override
+  _CompanyDesktopState createState() => _CompanyDesktopState();
+}
+
+class _CompanyDesktopState extends State<CompanyDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
