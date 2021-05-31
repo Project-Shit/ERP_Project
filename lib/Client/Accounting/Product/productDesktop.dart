f585fa9eea3f43b03b40351fa48f217c16be7830
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 20:30:35 +0200

    Update

--- lib/Client/Accounting/Product/productDesktop.dart
+++ lib/Client/Accounting/Product/productDesktop.dart
@@ -1,4 +1,6 @@
 // @dart=2.9
+import 'package:erp/constants.dart';
+import 'package:erp/widget/appBar/clientAppBar.dart';
 import 'package:flutter/material.dart';
 
 class ProductDesktop extends StatefulWidget {
@@ -8,8 +10,113 @@ class ProductDesktop extends StatefulWidget {
 }
 
 class _ProductDesktopState extends State<ProductDesktop> {
+  final _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: PrimaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size(width, 70),
+        child: ClientAppBar(),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            width: width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                50.0,
+              ),
+              border: Border.all(
+                color: TextColor,
+                width: 2,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                bottom: 50,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  labelText('Item ID'),
+                  Container(
+                    width: width,
+                    height: 45.0,
+                    child: TextFormField(
+                      style: TextStyle(
+                        color: TextColor,
+                      ),
+                      decoration: InputDecoration(
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(
+                            right: 10,
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {},
+                          ),
+                        ),
+                        fillColor: SecondaryColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            new Radius.circular(
+                              10.0,
+                            ),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            new Radius.circular(
+                              10.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Item Name'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Wholesale Price'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Price of Peace'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Price of Sale'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Profit'),
+                  textField(_textController, width, 45.0, false),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
