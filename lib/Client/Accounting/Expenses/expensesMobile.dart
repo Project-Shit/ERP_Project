b43a894e42bf795cc00bdd97c4ea201f44d9cec6
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 21:25:43 +0200

    Update

--- lib/Client/Accounting/Expenses/expensesMobile.dart
+++ lib/Client/Accounting/Expenses/expensesMobile.dart
@@ -1,3 +1,6 @@
+// @dart=2.9
+import 'package:erp/constants.dart';
+import 'package:erp/widget/drawer/clientDrawer.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesMobile extends StatefulWidget {
@@ -7,8 +10,117 @@ class ExpensesMobile extends StatefulWidget {
 }
 
 class _ExpensesMobileState extends State<ExpensesMobile> {
+
+  bool password = true;
+  final _textController = TextEditingController();
+  final _dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: PrimaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size(width, 60),
+        child: AppBar(
+          iconTheme: IconThemeData(
+            color: TextColor,
+          ),
+          backgroundColor: PrimaryColor,
+          title: Text(
+            'Company Name',
+            style: TextStyle(
+              color: TextColor,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+      drawer: ClientDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            width: width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                50.0,
+              ),
+              border: Border.all(
+                color: TextColor,
+                width: 2,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                bottom: 50,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  labelText('Date'),
+                  Container(
+                    width: width * 0.9,
+                    height: 45.0,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: SecondaryColor,
+                      ),
+                      readOnly: true,
+                      controller: _dateController,
+                      onTap: () async {
+                        var date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2100),
+                        );
+                        _dateController.text = date.toString().substring(0, 10);
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Total Income'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Expenses'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Equipment\'s Cost'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Repairing Cost'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Total Net Profit'),
+                  textField(_textController, width, 45.0, false),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
