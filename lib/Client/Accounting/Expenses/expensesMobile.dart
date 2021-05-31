b34c3ec2df1d753fec95e2e6fb29213878821b9b
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 21:02:40 +0200

    Create

---
+++ lib/Client/Accounting/Expenses/expensesMobile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ExpensesMobile extends StatefulWidget {
+
+  @override
+  _ExpensesMobileState createState() => _ExpensesMobileState();
+}
+
+class _ExpensesMobileState extends State<ExpensesMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
