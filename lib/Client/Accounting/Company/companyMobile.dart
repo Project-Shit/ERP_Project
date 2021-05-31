1fe9b678ce9e660fe0e29e2cfb311f4308569946
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-05-31 21:44:47 +0200

    Create

---
+++ lib/Client/Accounting/Company/companyMobile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CompanyMobile extends StatefulWidget {
+
+  @override
+  _CompanyMobileState createState() => _CompanyMobileState();
+}
+
+class _CompanyMobileState extends State<CompanyMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
