025894c0de88cda673f0486f83540759e9462bb9
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-02 10:24:35 +0200

    Update

---
+++ lib/Client/Accounting/Subscription/subscriptionMobile.dart
@@ -0,0 +1,191 @@
+// @dart=2.9
+import 'package:erp/constants.dart';
+import 'package:erp/widget/drawer/clientDrawer.dart';
+import 'package:flutter/material.dart';
+
+class SubscriptionMobile extends StatefulWidget {
+  @override
+  _SubscriptionMobileState createState() => _SubscriptionMobileState();
+}
+
+class _SubscriptionMobileState extends State<SubscriptionMobile> {
+  final _textController = TextEditingController();
+  final _dateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: PrimaryColor,
+      appBar: PreferredSize(
+        preferredSize: Size(width, 60),
+        child: AppBar(
+          iconTheme: IconThemeData(
+            color: TextColor,
+          ),
+          backgroundColor: PrimaryColor,
+          title: Text(
+            'Company Name',
+            style: TextStyle(
+              color: TextColor,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+        ),
+      ),
+      drawer: ClientDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            width: width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                50.0,
+              ),
+              border: Border.all(
+                color: TextColor,
+                width: 2,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                bottom: 50,
+                left: 20,
+                right: 20,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  labelText('Date'),
+                  Container(
+                    width: width * 0.9,
+                    height: 45.0,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: SecondaryColor,
+                      ),
+                      readOnly: true,
+                      controller: _dateController,
+                      onTap: () async {
+                        var date = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2100),
+                        );
+                        _dateController.text = date.toString().substring(0, 10);
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Client ID'),
+                  Container(
+                    width: width,
+                    height: 45,
+                    child: TextFormField(
+                      style: TextStyle(
+                        color: TextColor,
+                      ),
+                      decoration: InputDecoration(
+                        suffixIcon: Padding(
+                          padding: const EdgeInsets.only(
+                            right: 10,
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {},
+                          ),
+                        ),
+                        fillColor: SecondaryColor,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            new Radius.circular(
+                              10.0,
+                            ),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            new Radius.circular(
+                              10.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Client Name'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Client Phone'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Client Address'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Client Email'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Payment Method'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Card Number'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Subscription Type'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Subscription Plan'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Due Date'),
+                  textField(_textController, width, 45.0, false),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  labelText('Payment Date'),
+                  textField(_textController, width, 45.0, false),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
