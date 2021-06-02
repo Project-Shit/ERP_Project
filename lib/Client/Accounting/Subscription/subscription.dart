75155df01b84fb3499998306a182c41b9c8a83f4
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-02 22:20:43 +0200

    Update

--- lib/Client/Accounting/Subscription/subscription.dart
+++ lib/Client/Accounting/Subscription/subscription.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class Subscription extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
