01e1a3c2091f6edcf5857acbce32a0b900a2b665
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-04 17:02:50 +0200

    Update

--- lib/Client/Accounting/Users/users.dart
+++ lib/Client/Accounting/Users/users.dart
@@ -1,13 +1,10 @@
-// ignore: import_of_legacy_library_into_null_safe
+// @dart=2.9
 import 'package:erp/Client/Accounting/Users/usersDesktop.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:erp/Client/Accounting/Users/usersMobile.dart';
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:responsive_builder/responsive_builder.dart';
 
 class Users extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
