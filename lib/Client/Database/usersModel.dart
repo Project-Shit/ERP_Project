74b4b7e2a085ebf1c848e5cc5aa6eb753ed417fd
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-08 00:53:35 +0200

    Update

--- lib/Client/Database/usersModel.dart
+++ lib/Client/Database/usersModel.dart
@@ -1,28 +1,50 @@
 // @dart=2.9
+import 'dart:convert';
+
+List<UserModel> userModelFromJson(String str) => List<UserModel>.from(json.decode(str).map((x) => UserModel.fromJson(x)));
+
+String userModelToJson(List<UserModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class UserModel {
-  int id;
-  String name, phone, email, pass, address, department, userType;
+  UserModel({
+    this.id,
+    this.name,
+    this.phone,
+    this.email,
+    this.pass,
+    this.address,
+    this.department,
+    this.userType,
+  });
 
-  UserModel(
-      {this.id,
-      this.name,
-      this.phone,
-      this.email,
-      this.address,
-      this.department,
-      this.pass,
-      this.userType});
+  String id;
+  String name;
+  String phone;
+  String email;
+  String pass;
+  String address;
+  String department;
+  String userType;
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'] as int,
-      name: json['name'] as String,
-      phone: json['phone'] as String,
-      email: json['email'] as String,
-      pass: json['pass'] as String,
-      address: json['address'] as String,
-      department: json['department'] as String,
-      userType: json['userType'] as String,
-    );
-  }
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json["id"],
+    name: json["name"],
+    phone: json["phone"],
+    email: json["email"],
+    pass: json["pass"],
+    address: json["address"],
+    department: json["department"],
+    userType: json["userType"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "phone": phone,
+    "email": email,
+    "pass": pass,
+    "address": address,
+    "department": department,
+    "userType": userType,
+  };
 }
