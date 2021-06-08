7ab4b2550c73f2118f3724e8f05c3b231416d945
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-08 11:40:15 +0200

    Update

--- lib/Client/Database/usersModel.dart
+++ lib/Client/Database/usersModel.dart
@@ -1,50 +1,47 @@
 // @dart=2.9
-import 'dart:convert';
-
-List<UserModel> userModelFromJson(String str) => List<UserModel>.from(json.decode(str).map((x) => UserModel.fromJson(x)));
-
-String userModelToJson(List<UserModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
-
 class UserModel {
-  UserModel({
-    this.id,
-    this.name,
-    this.phone,
-    this.email,
-    this.pass,
-    this.address,
-    this.department,
-    this.userType,
-  });
+  final Map<String, dynamic> data;
+  final String id;
+  final String name;
+  final String phone;
+  final String email;
+  final String pass;
+  final String address;
+  final String department;
+  final String userType;
 
-  String id;
-  String name;
-  String phone;
-  String email;
-  String pass;
-  String address;
-  String department;
-  String userType;
+  UserModel(
+      {this.data,
+        this.id,
+        this.name,
+        this.phone,
+        this.email,
+        this.pass,
+        this.address,
+        this.department,
+        this.userType,
+      });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-    id: json["id"],
-    name: json["name"],
-    phone: json["phone"],
-    email: json["email"],
-    pass: json["pass"],
-    address: json["address"],
-    department: json["department"],
-    userType: json["userType"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "phone": phone,
-    "email": email,
-    "pass": pass,
-    "address": address,
-    "department": department,
-    "userType": userType,
-  };
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      data: {
+        'id': json['id'],
+        'name': json['name'],
+        'phone': json['phone'],
+        'email': json['email'],
+        'pass': json['pass'],
+        'address': json['address'],
+        'department': json['department'],
+        'userType': json['userType'],
+      },
+      id: json['id'],
+      name: json['name'],
+      phone: json['phone'],
+      email: json['email'],
+      pass: json['pass'],
+      address: json['address'],
+      department: json['department'],
+      userType: json['userType'],
+    );
+  }
 }
