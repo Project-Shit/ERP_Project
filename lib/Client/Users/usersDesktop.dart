2b888e843215ee2e58765fb819289956fa1f05eb
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-10 16:23:36 +0200

    Update

--- lib/Client/Users/usersDesktop.dart
+++ lib/Client/Users/usersDesktop.dart
@@ -27,6 +27,7 @@ class _UsersDesktopState extends State<UsersDesktop> {
   TextEditingController _address = TextEditingController();
   TextEditingController _department = TextEditingController();
   TextEditingController _userType = TextEditingController();
+
   // ignore: deprecated_member_use
   List _locations = List();
   String _selectedLocation;
@@ -85,49 +86,54 @@ class _UsersDesktopState extends State<UsersDesktop> {
     });
   }
 
-  setValue(){
-    String newValue;
-    setState(() {
-      _selectedLocation = newValue;
-      _searchController.text = newValue;
-    });
-  }
-
-  Future<Null> fetchData() async{
-    data = {"command": "select * from users where id = '${_searchController.text}'"};
-    return await http.post(Uri.parse(url2),body: data).then((http.Response response) {
-      final List fetchData = json.decode(response.body);
-      fetchData.forEach((user) {
-        setState(() {
-          _id.text = user['id'];
-          _name.text = user['name'];
-          _phone.text = user['phone'];
-          _email.text = user['email'];
-          _pass.text = user['pass'];
-          _address.text = user['address'];
-          _department.text = user['department'];
-          _userType.text = user['userType'];
+  // function to fetch data from database
+  Future<Null> fetchData() async {
+    try{
+      data = {
+        "command": "select * from users where id = '${_searchController.text}' or id = '${_selectedLocation.toString()}'"
+      };
+      return await http
+          .post(Uri.parse(url2), body: data)
+          .then((http.Response response) {
+        final List fetchData = json.decode(response.body);
+        fetchData.forEach((user) {
+          setState(() {
+            _id.text = user['id'];
+            _name.text = user['name'];
+            _phone.text = user['phone'];
+            _email.text = user['email'];
+            _pass.text = user['pass'];
+            _address.text = user['address'];
+            _department.text = user['department'];
+            _userType.text = user['userType'];
+          });
         });
       });
-    });
+    }catch (e){
+      showToast('Database Crash', position: ToastPosition.top);
+    }
   }
 
-  /*Future dropList() async {
-    data = {"command": "select id from users"};
-    http.post(Uri.parse(url), body: data).then((http.Response response) {
-      var fetchDecode = jsonDecode(response.body);
-      fetchDecode.forEach((users) {
-        setState(() {
-          _locations.add(users['id']);
+  Future dropList() async {
+    try{
+      data = {"command": "select id from users"};
+      http.post(Uri.parse(url2), body: data).then((http.Response response) {
+        var fetchDecode = jsonDecode(response.body);
+        fetchDecode.forEach((users) {
+          setState(() {
+            _locations.add(users['id']);
+          });
         });
-        print(_locations);
       });
-    });
-  }*/
+    } catch (e){
+        showToast('Database Crash', position: ToastPosition.top);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+    dropList();
   }
 
   @override
@@ -173,52 +179,6 @@ class _UsersDesktopState extends State<UsersDesktop> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // implementing a container to implement a search box
-                        Container(
-                          width: width,
-                          height: 40,
-                          child: TextFormField(
-                            controller: _searchController,
-                            style: TextStyle(
-                              color: textColor,
-                            ),
-                            decoration: InputDecoration(
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.only(
-                                  right: 10,
-                                ),
-                                child: IconButton(
-                                  icon: Icon(Icons.search),
-                                  onPressed: () {
-                                    fetchData();
-                                  },
-                                ),
-                              ),
-                              fillColor: secondaryColor,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  new Radius.circular(
-                                    10.0,
-                                  ),
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.all(
-                                  new Radius.circular(
-                                    10.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
@@ -265,7 +225,34 @@ class _UsersDesktopState extends State<UsersDesktop> {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
-                                textField(_id, width * 0.49, 40.0, true),
+                                Container(
+                                  width: width * 0.49,
+                                  height: 50.0,
+                                  child: DropdownButtonFormField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10.0),
+                                        ),
+                                      ),
+                                      filled: true,
+                                      fillColor: secondaryColor,
+                                    ),
+                                    value: _selectedLocation,
+                                    onChanged: (newValue) {
+                                      setState(() {
+                                        _selectedLocation = newValue;
+                                        fetchData();
+                                      });
+                                    },
+                                    items: _locations.map((location) {
+                                      return DropdownMenuItem(
+                                        child: new Text(location),
+                                        value: location,
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
                                 SizedBox(
                                   height: 15,
                                 ),
@@ -309,7 +296,9 @@ class _UsersDesktopState extends State<UsersDesktop> {
                           children: [
                             actionButtons('Apply', () {
                               _applyUser();
-                              showToast(message1? 'Applied':'Couldn\'t Apply', position: ToastPosition.top);
+                              showToast(
+                                  message1 ? 'Applied' : 'Couldn\'t Apply',
+                                  position: ToastPosition.top);
                               clear();
                             }, Colors.green),
                             SizedBox(
@@ -317,7 +306,9 @@ class _UsersDesktopState extends State<UsersDesktop> {
                             ),
                             actionButtons('Delete', () {
                               _delete();
-                              showToast(message2?'Deleted':'Couldn\'t Delete', position: ToastPosition.top);
+                              showToast(
+                                  message2 ? 'Deleted' : 'Couldn\'t Delete',
+                                  position: ToastPosition.top);
                               clear();
                             }, Colors.red.shade900),
                           ],
