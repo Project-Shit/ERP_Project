8e2f5f3d4f68766a95244560638c78fcf5341207
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-10 22:42:50 +0200

    Update

--- lib/Client/Users/usersDesktop.dart
+++ lib/Client/Users/usersDesktop.dart
@@ -18,29 +18,32 @@ class _UsersDesktopState extends State<UsersDesktop> {
   bool password = true;
   bool message1 = true;
   bool message2 = true;
-  TextEditingController _searchController = TextEditingController();
-  TextEditingController _id = TextEditingController();
   TextEditingController _name = TextEditingController();
   TextEditingController _phone = TextEditingController();
   TextEditingController _email = TextEditingController();
   TextEditingController _pass = TextEditingController();
   TextEditingController _address = TextEditingController();
-  TextEditingController _department = TextEditingController();
+  TextEditingController _selectedDept = TextEditingController();
   TextEditingController _userType = TextEditingController();
 
   // ignore: deprecated_member_use
   List _locations = List();
+
+  // ignore: deprecated_member_use
+  List _department = List();
+  // ignore: deprecated_member_use
+  List _user = List();
   String _selectedLocation;
   var setData = 'http://192.168.1.104/ERP/setAPI.php';
   var getData = 'http://192.168.1.104/ERP/getAPI.php';
   var data, response;
 
   // function to change values of a record
-  _applyUser() async {
+  applyUser() async {
     data = {
       "command": "update users set name = '${_name.text}', phone = '${_phone.text}',"
           " email = '${_email.text}' , pass = '${_pass.text}', address = '${_address.text}',"
-          "department = '${_department.text}', userType = '${_userType.text}' where id = ${_id.text}"
+          "department = '${_selectedDept.text}', userType = '${_userType.text}' where id = ${_selectedLocation.toString()}"
     };
     response = await http.post(Uri.parse(setData), body: data);
     if (200 == response.statusCode) {
@@ -51,11 +54,11 @@ class _UsersDesktopState extends State<UsersDesktop> {
   }
 
   // function to set record to null values
-  _delete() async {
+  delete() async {
     data = {
       "command": "update users set name = '', phone = '',"
           " email = '' , pass = '', address = '',"
-          "department = '', userType = '' where id = ${_id.text}"
+          "department = '', userType = '' where id = '${_selectedLocation.toString()}'"
     };
     response = await http.post(Uri.parse(setData), body: data);
     if (200 == response.statusCode) {
@@ -65,6 +68,18 @@ class _UsersDesktopState extends State<UsersDesktop> {
     }
   }
 
+  clear() {
+    setState(() {
+      _name.text = '';
+      _phone.text = '';
+      _email.text = '';
+      _pass.text = '';
+      _address.text = '';
+      _department = '' as List;
+      _userType.text = '';
+    });
+  }
+
   // function to change password field text's visibility
   hidePassword() {
     setState(() {
@@ -72,26 +87,11 @@ class _UsersDesktopState extends State<UsersDesktop> {
     });
   }
 
-  // function to clear all text fields
-  clear() {
-    setState(() {
-      _id.text = '';
-      _name.text = '';
-      _phone.text = '';
-      _email.text = '';
-      _pass.text = '';
-      _address.text = '';
-      _department.text = '';
-      _userType.text = '';
-    });
-  }
-
   // function to fetch data from database
   Future<Null> fetchData() async {
-    try {
       data = {
         "command":
-            "select * from users where id = '${_searchController.text}' or id = '${_selectedLocation.toString()}'"
+            "select * from users where id = '${_selectedLocation.toString()}'"
       };
       return await http
           .post(Uri.parse(getData), body: data)
@@ -99,25 +99,20 @@ class _UsersDesktopState extends State<UsersDesktop> {
         final List fetchData = json.decode(response.body);
         fetchData.forEach((user) {
           setState(() {
-            _id.text = user['id'];
             _name.text = user['name'];
             _phone.text = user['phone'];
             _email.text = user['email'];
             _pass.text = user['pass'];
             _address.text = user['address'];
-            _department.text = user['department'];
+            _selectedDept.text = user['department'];
             _userType.text = user['userType'];
           });
         });
       });
-    } catch (e) {
-      showToast('Database Crash', position: ToastPosition.top);
-    }
   }
 
   // function to set data to drop list
-  Future dropList() async {
-    try {
+  Future idList() async {
       data = {"command": "select id from users"};
       http.post(Uri.parse(getData), body: data).then((http.Response response) {
         var fetchDecode = jsonDecode(response.body);
@@ -127,15 +122,39 @@ class _UsersDesktopState extends State<UsersDesktop> {
           });
         });
       });
-    } catch (e) {
-      showToast('Database Crash', position: ToastPosition.top);
-    }
+  }
+
+  Future departmentList() async {
+      data = {"command": "select department from job"};
+      http.post(Uri.parse(getData), body: data).then((http.Response response) {
+        var fetchDecode = jsonDecode(response.body);
+        fetchDecode.forEach((dept) {
+          setState(() {
+            _department.add(dept['department']);
+          });
+        });
+      });
+  }
+
+  Future usersList() async {
+      data = {"command": "select userType from job where userType <> ''"};
+      http.post(Uri.parse(getData), body: data).then((http.Response response) {
+        var fetchDecode = jsonDecode(response.body);
+        fetchDecode.forEach((dept) {
+          setState(() {
+            _user.add(dept['userType']);
+          });
+        });
+      });
+
   }
 
   @override
   void initState() {
     super.initState();
-    dropList();
+    idList();
+    departmentList();
+    usersList();
   }
 
   @override
@@ -162,9 +181,7 @@ class _UsersDesktopState extends State<UsersDesktop> {
                 // implementing a container to make the outline border design
                 Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        50.0,
-                      ),
+                      borderRadius: BorderRadius.circular(50.0),
                       border: Border.all(
                         color: textColor,
                         width: 2,
@@ -271,7 +288,7 @@ class _UsersDesktopState extends State<UsersDesktop> {
                                   height: 15,
                                 ),
                                 passwordField(_pass, width * 0.49, 40.0,
-                                    password, true, hidePassword),
+                                    password, false, hidePassword),
                                 SizedBox(
                                   height: 15,
                                 ),
@@ -279,12 +296,74 @@ class _UsersDesktopState extends State<UsersDesktop> {
                                 SizedBox(
                                   height: 15,
                                 ),
-                                textField(
-                                    _department, width * 0.49, 40.0, true),
+                                Container(
+                                  width: width * 0.49,
+                                  height: 50.0,
+                                  child: Row(
+                                    children: <Widget>[
+                                      new Expanded(
+                                          child: new TextField(
+                                        controller: _selectedDept,
+                                            decoration: InputDecoration(
+                                              border: OutlineInputBorder(
+                                                borderRadius: BorderRadius.all(
+                                                  Radius.circular(10.0),
+                                                ),
+                                              ),
+                                              filled: true,
+                                              fillColor: secondaryColor,
+                                            ),
+                                      )),
+                                      new PopupMenuButton(
+                                        icon: const Icon(Icons.arrow_drop_down),
+                                        onSelected: (value) {
+                                          _selectedDept.text = value;
+                                        },
+                                        itemBuilder: (BuildContext context) {
+                                          return _department
+                                              .map<PopupMenuItem>((value) {
+                                            return PopupMenuItem(
+                                                child: new Text(value),
+                                                value: value);
+                                          }).toList();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
                                 SizedBox(
                                   height: 15,
                                 ),
-                                textField(_userType, width * 0.49, 40.0, true),
+                                Container(
+                                  width: width * 0.49,
+                                  height: 50.0,
+                                  child: Row(
+                                    children: <Widget>[
+                                      new Expanded(
+                                          child: new TextField(
+                                            controller: _userType,
+                                            decoration: InputDecoration(
+                                              border: OutlineInputBorder(
+                                                borderRadius: BorderRadius.all(
+                                                  Radius.circular(10.0),
+                                                ),
+                                              ),
+                                              filled: true,
+                                              fillColor: secondaryColor,
+                                            ),
+                                          )),
+                                      new PopupMenuButton(
+                                        icon: const Icon(Icons.arrow_drop_down),
+                                        onSelected: (value) {
+                                          _userType.text = value;
+                                        },
+                                        itemBuilder: (context) =>[
+
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
                           ],
@@ -297,17 +376,16 @@ class _UsersDesktopState extends State<UsersDesktop> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             actionButtons('Apply', () {
-                              _applyUser();
+                              applyUser();
                               showToast(
                                   message1 ? 'Applied' : 'Couldn\'t Apply',
                                   position: ToastPosition.top);
-                              clear();
                             }, Colors.green),
                             SizedBox(
                               width: 80,
                             ),
                             actionButtons('Delete', () {
-                              _delete();
+                              delete();
                               showToast(
                                   message2 ? 'Deleted' : 'Couldn\'t Delete',
                                   position: ToastPosition.top);
