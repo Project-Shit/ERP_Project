f335aec9e8283919f71337221bb46ce6871c635f
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-11 13:28:11 +0200

    Done

--- lib/Client/Users/usersMobile.dart
+++ lib/Client/Users/usersMobile.dart
@@ -1,7 +1,9 @@
 // @dart=2.9
+import 'dart:convert';
 import 'package:erp/constants.dart';
 import 'package:erp/widget/drawer/clientDrawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class UsersMobile extends StatefulWidget {
   @override
@@ -11,10 +13,20 @@ class UsersMobile extends StatefulWidget {
 // viewing all user page for the client's system
 class _UsersMobileState extends State<UsersMobile> {
   // objects implementation
-  List<String> _locations = [];
+  // ignore: deprecated_member_use
+  List _locations = List();
   String _selectedLocation;
   bool password = true;
-  final _textController = TextEditingController();
+  TextEditingController _name = TextEditingController();
+  TextEditingController _phone = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  TextEditingController _pass = TextEditingController();
+  TextEditingController _address = TextEditingController();
+  TextEditingController _department = TextEditingController();
+  TextEditingController _userType = TextEditingController();
+  var url = 'http://192.168.1.104/ERP/setAPI.php';
+  var url2 = 'http://192.168.1.104/ERP/getAPI.php';
+  var data, response;
 
   // function to change password field text's visibility
   void hidePassword() {
@@ -23,12 +35,47 @@ class _UsersMobileState extends State<UsersMobile> {
     });
   }
 
-  // function to change the value in the drop down list to the selected value
-  void setValue() {
-    String value = '';
-    setState(() {
-      _selectedLocation = value;
+  // function to fetch data from database
+  Future<Null> fetchData() async {
+    data = {
+      "command":
+          "select * from users where id = '${_selectedLocation.toString()}'"
+    };
+    return await http
+        .post(Uri.parse(url2), body: data)
+        .then((http.Response response) {
+      final List fetchData = json.decode(response.body);
+      fetchData.forEach((user) {
+        setState(() {
+          _name.text = user['name'];
+          _phone.text = user['phone'];
+          _email.text = user['email'];
+          _pass.text = user['pass'];
+          _address.text = user['address'];
+          _department.text = user['department'];
+          _userType.text = user['userType'];
+        });
+      });
     });
+  }
+
+  // function to set data to drop list
+  Future dropList() async {
+    data = {"command": "select id from users"};
+    http.post(Uri.parse(url2), body: data).then((http.Response response) {
+      var fetchDecode = jsonDecode(response.body);
+      fetchDecode.forEach((users) {
+        setState(() {
+          _locations.add(users['id']);
+        });
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dropList();
   }
 
   @override
@@ -86,43 +133,69 @@ class _UsersMobileState extends State<UsersMobile> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   labelText('ID'),
-                  dropList(_locations, _selectedLocation, width * 0.9, 45.0,
-                      setValue),
+                  Container(
+                    width: width,
+                    height: 55.0,
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: secondaryColor,
+                      ),
+                      value: _selectedLocation,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedLocation = newValue;
+                          fetchData();
+                        });
+                      },
+                      items: _locations.map((location) {
+                        return DropdownMenuItem(
+                          child: Text(location),
+                          value: location,
+                        );
+                      }).toList(),
+                    ),
+                  ),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Name'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_name, width, 50.0, false),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Phone'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_phone, width, 50.0, false),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Email'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_email, width, 50.0, false),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Password'),
-                  //passwordField(width * 0.9, 45.0, password, false, hidePassword),
+                  passwordField(_pass, width, 50.0, password, true, hidePassword),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Address'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_address, width, 50.0, false),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('Department'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_department, width, 50.0, false),
                   SizedBox(
                     height: 20,
                   ),
                   labelText('User Type'),
-                  textField(_textController, width, 45.0, false),
+                  textField(_userType, width, 50.0, false),
                 ],
               ),
             ),
