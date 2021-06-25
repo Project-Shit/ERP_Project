06ea9957d79289a32911efcc30d9a9eb4e4dafbe
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-25 15:50:13 +0200

    Update

--- lib/Client/LogIn/logInMobile.dart
+++ lib/Client/LogIn/logInMobile.dart
@@ -53,6 +53,7 @@ class _LogInMobileState extends State<LogInMobile> {
         ),
         child: Container(
           width: width,
+          height: 400,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(
               50.0,
@@ -88,29 +89,34 @@ class _LogInMobileState extends State<LogInMobile> {
                 passwordField(_passController, width, 45.0, password, false,
                     hidePassword),
                 SizedBox(
-                  height: 20,
+                  height: 35,
                 ),
-                Container(
-                  width: width * 0.8,
-                  height: 60,
-                  // ignore: deprecated_member_use
-                  child: RaisedButton(
-                    color: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: width * 0.6,
+                      height: 60,
+                      // ignore: deprecated_member_use
+                      child: RaisedButton(
+                        color: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(50),
+                          ),
+                        ),
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: textColor,
+                          ),
+                        ),
+                        onPressed: () {},
                       ),
                     ),
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: textColor,
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
+                  ],
+                )
               ],
             ),
           ),
