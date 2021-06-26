4d12471abb41f7a5eb7f3699a0849809d10f7131
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-06-26 22:35:28 +0200

    Update

--- lib/Client/LogIn/logInMobile.dart
+++ lib/Client/LogIn/logInMobile.dart
@@ -1,6 +1,11 @@
 // @dart=2.9
+import 'dart:convert';
+
+import 'package:erp/Client/Application/application.dart';
 import 'package:erp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:oktoast/oktoast.dart';
 
 class LogInMobile extends StatefulWidget {
   @override
@@ -11,6 +16,9 @@ class _LogInMobileState extends State<LogInMobile> {
   final _mailController = TextEditingController();
   final _passController = TextEditingController();
   bool password = true;
+  String userName = '';
+  var logInAPI = 'http://192.168.1.104/ERP/logIn.php';
+  var data, response;
 
   // function to change password field text's visibility
   void hidePassword() {
@@ -19,105 +27,133 @@ class _LogInMobileState extends State<LogInMobile> {
     });
   }
 
+  // function to log In
+  logIn() async {
+    try {
+      data = {
+        "mail": _mailController.text,
+        "pass": _passController.text,
+      };
+      response = await http.post(Uri.parse(logInAPI), body: data);
+      var user = json.decode(response.body);
+      if (user == 'Success') {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Application()));
+      } else {
+        showToast('Incorrect Email or Password', position: ToastPosition.top);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // Media Query object for responsive layout
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: primaryColor,
-      // implementing the client's AppBar
-      appBar: PreferredSize(
-        preferredSize: Size(width, 60),
-        child: AppBar(
-          iconTheme: IconThemeData(
-            color: textColor,
-          ),
-          backgroundColor: primaryColor,
-          title: Text(
-            'Company Name',
-            style: TextStyle(
+    return OKToast(
+      child: Scaffold(
+        backgroundColor: primaryColor,
+        // implementing the client's AppBar
+        appBar: PreferredSize(
+          preferredSize: Size(width, 60),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            iconTheme: IconThemeData(
               color: textColor,
-              fontSize: 25,
             ),
+            backgroundColor: primaryColor,
+            title: Text(
+              'Company Name',
+              style: TextStyle(
+                color: textColor,
+                fontSize: 25,
+              ),
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
         ),
-      ),
-      // implementing th body with scroll View and container widget
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: 20,
-          right: 20,
-          top: height * 0.15,
-        ),
-        child: Container(
-          width: width,
-          height: 400,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(
-              50.0,
-            ),
-            border: Border.all(
-              color: textColor,
-              width: 2,
-            ),
-          ),
+        // implementing th body with scroll View and container widget
+        body: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.only(
-              top: 50,
-              bottom: 50,
               left: 20,
               right: 20,
+              top: height * 0.1,
+              bottom: 20,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                labelText('Email'),
-                SizedBox(
-                  height: 15,
+            child: Container(
+              width: width,
+              height: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  50.0,
                 ),
-                textField(_mailController, width, 45.0, true),
-                SizedBox(
-                  height: 20,
+                border: Border.all(
+                  color: textColor,
+                  width: 2,
                 ),
-                labelText('Password'),
-                SizedBox(
-                  height: 15,
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 50,
+                  bottom: 50,
+                  left: 20,
+                  right: 20,
                 ),
-                passwordField(_passController, width, 45.0, password, false,
-                    hidePassword),
-                SizedBox(
-                  height: 35,
-                ),
-                Row(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      width: width * 0.6,
-                      height: 60,
-                      // ignore: deprecated_member_use
-                      child: RaisedButton(
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(50),
-                          ),
-                        ),
-                        child: Text(
-                          'Log In',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: textColor,
-                          ),
-                        ),
-                        onPressed: () {},
-                      ),
+                    labelText('Email'),
+                    SizedBox(
+                      height: 15,
                     ),
+                    textField(_mailController, width, 45.0, true),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    labelText('Password'),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    passwordField(_passController, width, 45.0, password, false,
+                        hidePassword),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: width * 0.6,
+                          height: 60,
+                          // ignore: deprecated_member_use
+                          child: RaisedButton(
+                            color: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50),
+                              ),
+                            ),
+                            child: Text(
+                              'Log In',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: textColor,
+                              ),
+                            ),
+                            onPressed: () {
+                              logIn();
+                            },
+                          ),
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
