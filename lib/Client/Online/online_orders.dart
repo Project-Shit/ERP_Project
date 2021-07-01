089402a29be1f3e30faef17211bd86b955a915af
Zeyadead1 <86671500+Zeyadead1@users.noreply.github.com>
2021-07-01 13:10:02 +0200

    Add

--- lib/Client/Online/online_orders.dart
+++ lib/Client/Online/online_orders.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(dat());
-
 }
 
-class dat extends StatelessWidget{
+class dat extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(
-            title: Text("Online Orders"),
+          appBar: AppBar(
 
-        ),
+            backgroundColor: Color.fromRGBO(32, 32, 32, 1),
+            title: Text(
+              "Online Orders",
+              style: TextStyle
+                (
+                fontSize: 30,
 
-      ),
+              )
 
+              ,
+            ),
+          ),
+          body: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("You Can Create order now"),
+              ],
+            ),
+          )),
     );
-
   }
-
-
 }
-
-
