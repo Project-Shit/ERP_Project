dbb7ca0c69f43d25b696f111476a16dd2222996f
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-02 20:49:33 +0200

    Create

---
+++ lib/Client/Accounting/Salary/salaryModel.dart
@@ -0,0 +1,22 @@
+// @dart=2.9
+class SalaryModel {
+  String id;
+  String name;
+  String department;
+  String salary;
+  String insurance;
+  String tax;
+  String deduction;
+  String netSalary;
+
+  SalaryModel({
+    this.id,
+    this.name,
+    this.department,
+    this.salary,
+    this.insurance,
+    this.tax,
+    this.deduction,
+    this.netSalary,
+  });
+}
