5f5416574d8bdc796f6e6056ca6f85b476bc9a64
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-03 01:18:14 +0200

    Update

--- lib/Client/Accounting/Company/companyDataTable.dart
+++ lib/Client/Accounting/Company/companyDataTable.dart
@@ -66,62 +66,66 @@ class _CompanyDataTableState extends State<CompanyDataTable> {
           backgroundColor: primaryColor,
           title: Image.asset(
             'assets/logo.png',
-            height: 70,
+            height: 50,
           ),
           centerTitle: true,
         ),
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              DataTable(
-                columns: [
-                  DataColumn(label: Text('Month')),
-                  DataColumn(label: Text('Year')),
-                  DataColumn(label: Text('Balance')),
-                  DataColumn(label: Text('Expenses')),
-                  DataColumn(label: Text('Salary')),
-                  DataColumn(label: Text('Income')),
-                  DataColumn(label: Text('Tax')),
-                  DataColumn(label: Text('New Balance')),
-                  DataColumn(label: Text('Profit')),
-                ],
-                rows: model
-                    .map((data) => DataRow(
-                          cells: [
-                            new DataCell(
-                              Text(data.month),
-                            ),
-                            new DataCell(
-                              Text(data.year),
-                            ),
-                            new DataCell(
-                              Text(data.balance),
-                            ),
-                            new DataCell(
-                              Text(data.expenses),
-                            ),
-                            new DataCell(
-                              Text(data.salary),
-                            ),
-                            new DataCell(
-                              Text(data.income),
-                            ),
-                            new DataCell(
-                              Text(data.tax),
-                            ),
-                            new DataCell(
-                              Text(data.newBalance),
-                            ),
-                            new DataCell(
-                              Text(data.profit),
-                            ),
-                          ],
-                        ))
-                    .toList(),
-              ),
-            ],
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                DataTable(
+                  columns: [
+                    DataColumn(label: Text('Month')),
+                    DataColumn(label: Text('Year')),
+                    DataColumn(label: Text('Balance')),
+                    DataColumn(label: Text('Expenses')),
+                    DataColumn(label: Text('Salary')),
+                    DataColumn(label: Text('Income')),
+                    DataColumn(label: Text('Tax')),
+                    DataColumn(label: Text('New Balance')),
+                    DataColumn(label: Text('Profit')),
+                  ],
+                  rows: model
+                      .map((data) => DataRow(
+                            cells: [
+                              new DataCell(
+                                Text(data.month),
+                              ),
+                              new DataCell(
+                                Text(data.year),
+                              ),
+                              new DataCell(
+                                Text(data.balance),
+                              ),
+                              new DataCell(
+                                Text(data.expenses),
+                              ),
+                              new DataCell(
+                                Text(data.salary),
+                              ),
+                              new DataCell(
+                                Text(data.income),
+                              ),
+                              new DataCell(
+                                Text(data.tax),
+                              ),
+                              new DataCell(
+                                Text(data.newBalance),
+                              ),
+                              new DataCell(
+                                Text(data.profit),
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
