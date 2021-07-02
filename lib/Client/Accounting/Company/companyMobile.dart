56bafd2fc5aba6c02d2a2be97dc6ed801f29ba7b
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-03 01:18:24 +0200

    Update

--- lib/Client/Accounting/Company/companyMobile.dart
+++ lib/Client/Accounting/Company/companyMobile.dart
@@ -1,5 +1,6 @@
 // @dart=2.9
 import 'dart:convert';
+import 'package:erp/Client/Accounting/Company/companyDataTable.dart';
 import 'package:erp/constants.dart';
 import 'package:erp/widget/drawer/clientDrawer.dart';
 import 'package:flutter/material.dart';
@@ -100,12 +101,9 @@ class _CompanyMobileState extends State<CompanyMobile> {
             color: textColor,
           ),
           backgroundColor: primaryColor,
-          title: Text(
-            'Company Name',
-            style: TextStyle(
-              color: textColor,
-              fontSize: 25,
-            ),
+          title: Image.asset(
+            'assets/logo.png',
+            height: 50,
           ),
           centerTitle: true,
         ),
@@ -129,8 +127,8 @@ class _CompanyMobileState extends State<CompanyMobile> {
             ),
             child: Padding(
               padding: EdgeInsets.only(
-                top: 50,
-                bottom: 50,
+                top: 30,
+                bottom: 30,
                 left: 20,
                 right: 20,
               ),
@@ -243,6 +241,40 @@ class _CompanyMobileState extends State<CompanyMobile> {
                   ),
                   labelText('Profit'),
                   textField(_profitController, width, 45.0, false),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: width * 0.6,
+                        height: 60,
+                        // ignore: deprecated_member_use
+                        child: RaisedButton(
+                          color: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50),
+                            ),
+                          ),
+                          child: Text(
+                            'Data Table',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: textColor,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => CompanyDataTable()));
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
