a48e05289955d29b2ae607879e19cce1a3d5bd53
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-04 19:53:47 +0200

    Update

--- lib/Client/LogIn/logInDesktop.dart
+++ lib/Client/LogIn/logInDesktop.dart
@@ -75,13 +75,10 @@ class _LogInDesktopState extends State<LogInDesktop> {
           iconTheme: IconThemeData(
             color: textColor,
           ),
-          backgroundColor: primaryColor,
-          title: Text(
-            'Company Name',
-            style: TextStyle(
-              color: textColor,
-              fontSize: 25,
-            ),
+          backgroundColor: secondaryColor,
+          title: Image.asset(
+            'assets/logo.png',
+            height: 50,
           ),
           centerTitle: true,
         ),
@@ -96,8 +93,6 @@ class _LogInDesktopState extends State<LogInDesktop> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  width: width * 0.4,
-                  height: 400,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(
                       50.0,
@@ -108,22 +103,21 @@ class _LogInDesktopState extends State<LogInDesktop> {
                     ),
                   ),
                   child: Padding(
-                    padding: EdgeInsets.all(50),
+                    padding: EdgeInsets.all(30),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         SizedBox(
                           height: 15,
                         ),
-                        textField(_mailController, width, 45.0, true,'Email'),
+                        textField(_mailController, width * 0.3, 45.0, true,'Email'),
                         SizedBox(
                           height: 20,
                         ),
                         SizedBox(
                           height: 15,
                         ),
-                        passwordField(_passController, width, 45.0, password,
+                        passwordField(_passController, width * 0.3, 45.0, password,
                             false, hidePassword),
                         SizedBox(
                           height: 35,
