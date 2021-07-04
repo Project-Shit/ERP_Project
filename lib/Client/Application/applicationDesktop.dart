fa20aa523b46a2e6b09180faa76a5ca435a99229
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-04 19:53:41 +0200

    Update

--- lib/Client/Application/applicationDesktop.dart
+++ lib/Client/Application/applicationDesktop.dart
@@ -25,88 +25,102 @@ class ApplicationDesktop extends StatelessWidget {
         // ignore: missing_return
         onWillPop: () {},
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: 150,
-              right: 150,
-              top: 80,
-              bottom: 30,
-            ),
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 150,
+                  right: 150,
+                  top: 80,
+                  bottom: 30,
+                ),
 
-            /* implementing a column widget to contain all widget inside of it,
-            *  which include a custom card widget for each application in
-            *  the client's system, each 4 card widget are set together inside
-            *  a row, and passing the parameters for each card => title, width,
-            *  height and the onTap action, with sizedBox between each widget
-            *  to give it a good looking view. */
-            child: Column(
-              children: [
-                Row(
+                /* implementing a column widget to contain all widget inside of it,
+                *  which include a custom card widget for each application in
+                *  the client's system, each 4 card widget are set together inside
+                *  a row, and passing the parameters for each card => title, width,
+                *  height and the onTap action, with sizedBox between each widget
+                *  to give it a good looking view. */
+                child: Column(
                   children: [
-                    applicationCard(
-                      'Users',
-                      width * 0.18,
-                      100,
-                      () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Users(),
+                    Row(
+                      children: [
+                        Container(
+                          child: applicationCard(
+                            'Users',
+                            width * 0.18,
+                            100,
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Users(),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
-                    ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    applicationCard(
-                      'Salary',
-                      width * 0.18,
-                      100,
-                      () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Salary(),
+                        ),
+                        SizedBox(
+                          width: width * 0.02,
+                        ),
+                        Container(
+                          child: applicationCard(
+                            'Salary',
+                            width * 0.18,
+                            100,
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Salary(),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
-                    ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    applicationCard(
-                      'Company',
-                      width * 0.18,
-                      100,
-                      () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Company(),
+                        ),
+                        SizedBox(
+                          width: width * 0.02,
+                        ),
+                        Container(
+                          child: applicationCard(
+                            'Company',
+                            width * 0.18,
+                            100,
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Company(),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
-                    ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    applicationCard(
-                      'Online Order',
-                      width * 0.18,
-                      100,
-                      () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => OnlineOrder(),
+                        ),
+                        SizedBox(
+                          width: width * 0.02,
+                        ),
+                        Container(
+                          child: applicationCard(
+                            'Online Order',
+                            width * 0.18,
+                            100,
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => OnlineOrder(),
+                                ),
+                              );
+                            },
                           ),
-                        );
-                      },
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
