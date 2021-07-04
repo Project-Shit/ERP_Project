22376312ee1bdfd2e44dd0962140a74625fe00bc
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-04 02:01:15 +0200

    Update

--- lib/Client/Online/online_orders.dart
+++ lib/Client/Online/online_orders.dart
@@ -2,81 +2,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class OnlineOrder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-             backgroundColor: Color.fromRGBO(32, 32, 32, 1),
+            backgroundColor: Color.fromRGBO(32, 32, 32, 1),
             title: Text(
               "Online Orders",
-              style: TextStyle
-                (
+              style: TextStyle(
                 fontSize: 40,
-
-              )
-
-              ,
+              ),
             ),
           ),
           body: Container(
-
-            child: Column(
-
-                    children: <Widget>
-                    [
-
-                      SizedBox(width: 120,),
-
-                      Text("Customer Info",style:TextStyle(
-                        fontSize: 30,
-
-
-                      )
-                        ,),
-                      SizedBox(width: 120,),
-                      SizedBox(width: 120,),
-
-
-                      Container(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            enabledBorder : OutlineInputBorder(
-                            borderSide: BorderSide(width: 2 , color: Color.fromRGBO(32, 32, 32, 1)),
-
-                            ),
-                            labelText: "Enter Customer Phone Number",
-                            icon: Icon(Icons.phone),
-                          ),
-
-
-
-                        ),
-
-
-                      ),
-                      OutlinedButton(onPressed: null ,
-
-                          child: Text("Next",
-                            style: TextStyle(color: Color.fromRGBO(32, 32, 32, 1)
-                            ),
-                          )
-                      ),
-
-
-                    ]
-
-
-            ),
-
-
-          )
-
-
-
-      ),
+            child: Column(children: <Widget>[
+              SizedBox(
+                width: 120,
+              ),
+              Text(
+                "Customer Info",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                width: 120,
+              ),
+              SizedBox(
+                width: 120,
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 2, color: Color.fromRGBO(32, 32, 32, 1)),
+                    ),
+                    labelText: "Enter Customer Phone Number",
+                    icon: Icon(Icons.phone),
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: null,
+                  child: Text(
+                    "Next",
+                    style: TextStyle(color: Color.fromRGBO(32, 32, 32, 1)),
+                  )),
+            ]),
+          )),
     );
   }
 }
