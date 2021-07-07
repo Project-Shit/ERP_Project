f66bd53c90148c2f499c7a9b390619f630dfa926
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-08 00:18:06 +0200

    Update

--- lib/Client/Online/online_orders.dart
+++ lib/Client/Online/online_orders.dart
@@ -4,105 +4,84 @@ import 'package:erp/widget/chat/chatButton.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
-
-
-void main(){
-runApp(OnlineOrder());
-
+void main() {
+  runApp(OnlineOrder());
 }
-class OnlineOrder extends StatefulWidget{
+
+class OnlineOrder extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return OnlineOrderState();
   }
-
 }
 
-class OnlineOrderState extends State<OnlineOrder>{
+class OnlineOrderState extends State<OnlineOrder> {
   final _test = TextEditingController();
 
-
-
-
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(32, 32, 32, 1),
-            title: Text(
-              "Online Orders",
-              style: TextStyle(
-                fontSize: 40,
-              ),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(32, 32, 32, 1),
+          title: Text(
+            "Online Orders",
+            style: TextStyle(
+              fontSize: 40,
             ),
           ),
-          body: Container(
-            width: double.infinity,
-            margin: EdgeInsets.all(40.0),
-            child: Column(children: <Widget>[
-              SizedBox(
-                width: 120,
+        ),
+        body: Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(40.0),
+          child: Column(children: <Widget>[
+            SizedBox(
+              width: 120,
+            ),
+            Text(
+              "Customer Info",
+              style: TextStyle(
+                fontSize: 30,
               ),
-              Text(
-                "Customer Info",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              SizedBox(
-                width: 120,
-              ),
-              SizedBox(
-                width: 120,
-              ),
-              Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2, color: Color.fromRGBO(32, 32, 32, 1)),
-                    ),
-                    labelText: "Enter Customer Phone Number",
-                    icon: Icon(Icons.phone),
+            ),
+            SizedBox(
+              width: 120,
+            ),
+            SizedBox(
+              width: 120,
+            ),
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 2, color: Color.fromRGBO(32, 32, 32, 1)),
                   ),
+                  labelText: "Enter Customer Phone Number",
+                  icon: Icon(Icons.phone),
                 ),
-
-
               ),
-              Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2, color: Color.fromRGBO(32, 32, 32, 1)),
-                    ),
-                    labelText: "Name",
-                    icon: Icon(Icons.person),
+            ),
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 2, color: Color.fromRGBO(32, 32, 32, 1)),
                   ),
+                  labelText: "Name",
+                  icon: Icon(Icons.person),
                 ),
-
-
-
-
-
               ),
-
-            textField(_test, 250, 60.0, true,'test'),
-
-
-
-
-              OutlinedButton(
-                  onPressed: null,
-                  child: Text(
-                    "Next",
-                    style: TextStyle(color: Color.fromRGBO(32, 32, 32, 1)),
-                  )),
-            ]),
-          ),
+            ),
+            textField(_test, 250, 60.0, true, 'test'),
+            OutlinedButton(
+                onPressed: null,
+                child: Text(
+                  "Next",
+                  style: TextStyle(color: Color.fromRGBO(32, 32, 32, 1)),
+                )),
+          ]),
+        ),
         floatingActionButton: ChatButton(),
       ),
     );
